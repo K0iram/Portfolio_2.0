504bc9f78cd389b145c212fba447fd6911d0a624
mario7746 <m.kennedykavouras@gmail.com>
2018-01-25 17:30:01 -0800

    Changes to hashrouter to fix the reload problem when deployed

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import Router from 'react-router-dom/BrowserRouter'
+import Router from 'react-router-dom/HashRouter'
 import Route from 'react-router-dom/Route'
+import Switch from 'react-router-dom/Switch'
+
 
 
 import AppLayout from './layouts/main'
@@ -13,11 +15,13 @@ import About from './components/About'
 const Routes = () => (
   <Router>
     <AppLayout>
+    	<Switch>
         <Route path="/" exact component={Home} />
         <Route path="/home" component={Home} />
         <Route path="/portfolio" component={Portfolio} />
         <Route path="/about" component={About} />
         <Route path="/contact" component={Contact} />
+      </Switch>
     </AppLayout>
   </Router>
 )
