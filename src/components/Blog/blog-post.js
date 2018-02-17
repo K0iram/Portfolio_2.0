37445d644ba6e6c4c23ec24e3f456e902d352ba2
dimitri Kennedy <dimitrikennedy@gmail.com>
2018-02-16 22:18:13 -0500

    remove logic from post

--- src/components/Blog/blog-post.js
+++ src/components/Blog/blog-post.js
@@ -2,46 +2,27 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 import { resolveImageUrl } from 'api/posts'
+import ContentfulImage from 'components/ContentfulImage'
 import './style.css'
 
-class BlogPost extends Component {
+const BlogPost = (props) => {
+	const { title, id, fields } = props
+	const { featuredImage } = fields
 
-	state = {
-		featuredImageUrl: 'http://putadefultimagehere.jpg'
-	}
-
-	async componentDidMount() {
-		await this.resolveFeaturedImage()
-	}
-
-	resolveFeaturedImage = async () => {
-		const { featuredImage } = this.props.fields
-		const { data } = await resolveImageUrl(featuredImage.sys.id)
-
-		this.setState({
-			featuredImageUrl: data.fields.file.url
-		})
-	}
-
-	render() {
-		const { title, id } = this.props
-		const { featuredImageUrl } = this.state
-
-		return(
-			<div className="blog-post">
-				<Link to={`/blog/${id}`}>
-					<div className="blog-post__img">
-						<img src={featuredImageUrl} alt="change me later"/>
-					</div>
-					<div className="blog-post__title">
-						<h5>
-							{title}
-						</h5>
-					</div>
-				</Link>
-			</div>
-		)
-	}
+	return(
+		<div className="blog-post">
+			<Link to={`/blog/${id}`}>
+				<div className="blog-post__img">
+					<ContentfulImage id={featuredImage.sys.id} alt="change me later" />
+				</div>
+				<div className="blog-post__title">
+					<h5>
+						{title}
+					</h5>
+				</div>
+			</Link>
+		</div>
+	)
 }
 
 export default BlogPost
