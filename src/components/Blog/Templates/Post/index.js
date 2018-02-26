0f284f1ef6a6455b707a35d5c51e0769d9104dbe
mario7746 <m.kennedykavouras@gmail.com>
2018-02-26 13:34:34 -0800

    Adds marked package for markdown conversion. Next I need to maked the markdown its own component.

--- src/components/Blog/Templates/Post/index.js
+++ src/components/Blog/Templates/Post/index.js
@@ -1,46 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import marked from 'marked'
 import arrow from './icons/back.svg'
 import ContentfulImage from 'components/ContentfulImage'
 import './style.css'
 
-const Post = props => {
-	const { title, description, body, secondBody, links, media, heroImage } = props
+class Post extends Component {
+  getMarkdownText(text) {
+      var rawMarkup = marked(text, {sanitize: true});
+      return { __html: rawMarkup };
+  }
 
-	return (
-		<div>
-			<Link to="/blog" className='back-link'>
-				<img src={arrow} alt="back-arrow"/>
-				<p>Back to all posts</p>
-			</Link>
-			<div className="blog-post__container">
-				<div className="blog-post__title">
-					<h1>{title}</h1>
-					<h3>{description}</h3>
-				</div>
-				<figure className="blog-post__hero">
-					<img src={heroImage} alt="main-hero-image"/>
-				</figure>
-				<div className="blog-post__body">
-					<p>{body}</p>
-				</div>
-				<div className="blog-post__gallery">
-					{media.map((img, i) => <ContentfulImage id={img.sys.id} key={`img-${i}`}/>)}
-				</div>
-				{secondBody !== '' &&
-				<div className="blog-post__body">
-					<p>{secondBody}</p>
-				</div>
-				}
-				<div className="helpful-links">
-					<h5>Check out these links</h5>
-					<ul>
-						<li><a href={links}>THIS IS A LINK</a></li>
-					</ul>
+	render() {
+		const { title, description, body, secondBody, links, media, heroImage } = this.props
+
+		return (
+			<div>
+				<Link to="/blog" className='back-link'>
+					<img src={arrow} alt="back-arrow"/>
+					<p>Back to all posts</p>
+				</Link>
+				<div className="blog-post__container">
+					<div className="blog-post__title">
+						<h1>{title}</h1>
+						<h3>{description}</h3>
+					</div>
+					<figure className="blog-post__hero">
+						<img src={heroImage} alt="main-hero-image"/>
+					</figure>
+					<div className="blog-post__body">
+						<div dangerouslySetInnerHTML={this.getMarkdownText(body)} />
+					</div>
+					<div className="blog-post__gallery">
+						{media.map((img, i) => <ContentfulImage id={img.sys.id} key={`img-${i}`}/>)}
+					</div>
 				</div>
 			</div>
-		</div>
-	)
+		)
+	}
 }
 
 export default Post
