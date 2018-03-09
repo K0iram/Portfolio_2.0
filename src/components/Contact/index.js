fc337e14d268d962d00e14995b9022117d2dc74b
mario7746 <m.kennedykavouras@gmail.com>
2018-03-09 11:33:46 -0800

    Removes phone number from contact page. Adds google map. Adds normal drift widget to contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -6,44 +6,36 @@ import './style.css'
 
 class Contact extends Component {
 
-	componentWillUnmount() {
-		window.drift.hide()
-	}
+  componentDidMount() {
+    window.drift.show()
+  }
 
-	openDrift = () => {
-		window.drift.on('ready',function(api, payload) {
-			api.sidebar.open()
-		})
-	}
+  componentWillUnmount() {
+    window.drift.hide()
+  }
 
 	render() {
-		return (
-			<div className="contact-container">
-				<div className="contact-hero">
-					<img src="https://s3.amazonaws.com/mariosbucket/deskPic.jpeg" alt="desk"/>
-					<h5>Contact Me</h5>
-				</div>
-	
-				<div className="contact-item">
-					<a href="mailto:mk@mariokennedy.com">mk@mariokennedy.com</a>
-				</div>
-				<div className="contact-item">
-					<a href="tel:7747221431">774-722-1431</a>
-				</div>
-				<div className="contact-item">
-					<a 
-						href="https://www.google.com/maps/place/San+Diego,+CA/@32.7549719,-117.1972497,12z/data=!4m13!1m7!3m6!1s0x80d9530fad921e4b:0xd3a21fdfd15df79!2sSan+Diego,+CA!3b1!8m2!3d32.715738!4d-117.1610838!3m4!1s0x80d9530fad921e4b:0xd3a21fdfd15df79!8m2!3d32.715738!4d-117.1610838"
-						target="_blank"
-					>
-						San Diego, CA 92101
-					</a>
-				</div>
-				<div className="contact-item">
-					<button onClick={this.openDrift}>Chat Now!</button>
-				</div>
-			</div>
-		)
-	}
+    return (
+      <div className="contact-container">
+        <div className="contact-hero">
+          <img src="https://s3.amazonaws.com/mariosbucket/deskPic.jpeg" alt="desk"/>
+          <h5>Contact Me</h5>
+        </div>
+
+        <div className="contact-item">
+          <a href="mailto:mk@mariokennedy.com">mk@mariokennedy.com</a>
+        </div>
+        <div className="contact-item">
+          <a
+          href="https://www.google.com/maps/place/San+Diego,+CA/@32.7549719,-117.1972497,12z/data=!4m13!1m7!3m6!1s0x80d9530fad921e4b:0xd3a21fdfd15df79!2sSan+Diego,+CA!3b1!8m2!3d32.715738!4d-117.1610838!3m4!1s0x80d9530fad921e4b:0xd3a21fdfd15df79!8m2!3d32.715738!4d-117.1610838"
+          target="_blank"
+          >
+          San Diego, CA 92101
+          </a>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Contact;
