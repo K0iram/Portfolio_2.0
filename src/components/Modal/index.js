0f4b28d6b43ddcc9824601a2f2463ad2292dbf91
mario7746 <m.kennedykavouras@gmail.com>
2018-06-13 21:02:54 -0700

    Adds buttons to the modal instead of just links.

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -34,9 +34,9 @@ class PreviewModal extends Component {
                 </ul>
               </div>
               <div className="project-links">
-                <a href={url} target="_blank">Check out the site</a>
+                <a href={url} target="_blank"><button>Check out the site</button></a>
                 {!!codeUrl &&
-                  <a href={codeUrl} target="_blank">Check out the code</a>
+                  <a href={codeUrl} target="_blank"><button>Check out the code</button></a>
                 }
               </div>
             </div>
